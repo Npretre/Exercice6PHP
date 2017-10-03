0b817100f9615e2ebd148189b62ff983a873d425
Nicolas Pretre <nicolas.pretre.dev@gmail.com>
2017-10-03 15:38:47 +0200

    Correction

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-$langage = array ("HTML","CSS","Javascript","PHP");
-sort($langage);
-echo implode("</br>", $langage);
+$langage = array('HTML','CSS','Javascript','PHP');
+function alpha($code){
+  if (is_array($code)==true) {
+    sort($code);
+  }
+  return $code;
+}
+$sortedarray = alpha($langage);
+foreach ($sortedarray as $value) {
+  echo $value.'<br />';
+}
+
+function concat($code){
+  if (is_array($code)==true) {
+    $string = implode(' ', $code);
+  }
+  return $string;
+}
+echo concat($langage);
 ?>
